1d6616e97ff57ac32f48ed502e6f459f17760360
Edward 'Ted <159036536+Swillycoder@users.noreply.github.com>
2025-05-28 13:12:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ canvas.height = 608;
 //const map = new Image();
 //map.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/map.png';
 
+const agaric = new Image();
+agaric.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/agaricpixel.png';
+
+const shroomblue = new Image();
+shroomblue.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroombluepixel.png';
+
+const shroompurple = new Image();
+shroompurple.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroompurplepixel.png';
+
+const oil = new Image();
+oil.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/oil.png';
+
+const toxic = new Image();
+toxic.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/toxic.png';
+
+const radiation = new Image();
+radiation.src = 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/radiation.png';
+
+map.onload = () => {map.loaded = true;};
+agaric.onload = () => {agaric.loaded = true;};
+shroomblue.onload = () => {shroomblue.loaded = true;};
+shroompurple.onload = () => {shroompurple.loaded = true;};
+oil.onload = () => {oil.loaded = true;};
+toxic.onload = () => {toxic.loaded = true;};
+radiation.onload = () => {radiation.loaded = true;};
+
 const images = {
     shroom: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroomback.png',
     map: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/map.png',
@@ -15,14 +41,12 @@ const images = {
     //spores: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/spores.png',
     //sporeprint: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/sporeprint.png',
     //log: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/log32px.png',
-    oil: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/oil.png',
-    toxic: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/toxic.png',
-    radiation: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/radiation.png',
-
-    agaric: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/agaricpixel.png',
-    shroomblue: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroombluepixel.png',
-    shroompurple: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroompurplepixel.png',
-
+    //oil: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/oil.png',
+    //toxic: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/toxic.png',
+    //radiation: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/radiation.png',
+    //agaric: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/agaricpixel.png',
+    //shroomblue: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroombluepixel.png',
+    //shroompurple: 'https://raw.githubusercontent.com/Swillycoder/shroomrunner/main/shroompurplepixel.png',
 };
 
 const loadImage = (src) => {
@@ -177,12 +201,12 @@ let speedIncreaseInterval = 600;
 
 
 function getRandomImage() {
-    const randImage = [loadedImages.oil, loadedImages.toxic, loadedImages.radiation];
+    const randImage = [oil, toxic, radiation];
     return randImage[Math.floor(Math.random() * 3)];
 }
 
 function getRandomReward() {
-    const randReward = [loadedImages.agaric, loadedImages.shroomblue, loadedImages.shroompurple];
+    const randReward = [agaric, shroomblue, shroompurple];
     return randReward[Math.floor(Math.random() * 3)];
 }
 
@@ -264,12 +288,12 @@ let backgroundY = 608;
 
 function gameScreen() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.drawImage(map, 0, backgroundY);
+    ctx.drawImage(map, 0, backgroundY);
 
-    //backgroundY += backgroundSpeed;
-    //if (backgroundY >= 0) {
-    //    backgroundY = -608;
-    //}
+    backgroundY += backgroundSpeed;
+    if (backgroundY >= 0) {
+        backgroundY = -608;
+    }
 
     player.update();
 
